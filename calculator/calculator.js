079d834080f8590d8a818f97c1793012a9ae7ccc
AndrewGalkin <127788127788a@gmail.com>
2023-01-06 15:24:36 +0200

    calculator(unfinished)

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -33,26 +33,97 @@ function check(el){
         break;
     }
 }
+
 //что бы подсчитать разбей на элементы по типу +- и тд
 //можно по отдельным функциям
-var result = 0;
-function mathAction() {
-  var number;
-  if(!document.getElementById("conclusion").value == 0){
-    if(document.getElementById('plus').onclick){
-      number = 
-      result += Number(document.getElementById("conclusion").value);
-      console.log(result);
-      document.getElementById("conclusion").value = "";
-    }
-    if(document.getElementById('minus').onclick){
-
-      result -= Number(document.getElementById("conclusion").value);
-      console.log(result);
-      document.getElementById("conclusion").value = "";
-    }
-  }
-}
-  // var result = document.getElementById("conclusion").value;
-  // result *= Number(document.getElementById("conclusion").value);
-  // document.getElementById("conclusion").value = "";
+//все что снизу нерабочие варианты
+// function plus() {
+//   var number1;
+//   var number2;
+//   if(!document.getElementById("conclusion").value == 0){
+//     //if(document.getElementById("plus").onclick){
+//       number = Number(document.getElementById("conclusion").value);
+//       result += number;
+//       document.getElementById("conclusion").value = "";
+//       number2 = Number(document.getElementById("conclusion").value);
+//       console.log(result);
+//   //  }
+//     // if(document.getElementById("minus").onclick){
+//     //   number = Number(document.getElementById("conclusion").value)
+//     //   result += number;
+//     //   console.log(result);
+//     //   document.getElementById("conclusion").value = "";
+//     // }
+//   }
+// }
+//   // var result = document.getElementById("conclusion").value;
+//   // result *= Number(document.getElementById("conclusion").value);
+//   // document.getElementById("conclusion").value = "";
+// function minus(){
+//   if(!document.getElementById("conclusion").value == 0){
+//     var number1;
+//     var number2;
+//     number1 = Number(document.getElementById("conclusion").value);
+//     console.log(number1);
+//     document.getElementById("conclusion").value = "";
+//     do{
+//     number2 = Number(document.getElementById("conclusion").value);
+//     console.log(number2);
+//     }while(number2 == "")
+//     result = number1 - number2;
+//
+//   }
+// }
+// function plus(){
+//   var number = 0;
+//   if(document.getElementById('plus').onclick){
+//     if(!document.getElementById('conclusion').value == ""){
+//       var number = Number(document.getElementById('conclusion').value);
+//     }
+//     if(!document.getElementById('conclusion').value == ""){
+//       result += number;
+//       console.log(result);
+//     }
+//     document.getElementById('conclusion').value = "";
+//   }
+// }
+// function minus(){
+//   var number = 0;
+//   if(document.getElementById('minus').onclick){
+//     if(!document.getElementById('conclusion').value == ""){
+//       var number = Number(document.getElementById('conclusion').value);
+//     }
+//     if(!document.getElementById('conclusion').value == ""){
+//       result -= number;
+//       console.log(result);
+//     }
+//     document.getElementById('conclusion').value = "";
+//   }
+// }
+//нерабочий вариант
+//разделить функции получения значения и мат действий(нерабочий)
+// var result = 0;
+// var plus1 = new Array();
+// function plus(){
+//   conclusion.value += "+";
+// }
+// function minus(){
+//   conclusion.value += "-";
+// }
+// function multiplication(){
+//   conclusion.value += "*";
+// }
+// function division(){
+//   conclusion.value += "/";
+// }
+// function modulo(){
+//   conclusion.value += "%";
+// }
+// function calculate(){
+//   plus1 = conclusion.value;
+//   console.log(plus1);
+//   plus1 = plus1.split('+');
+//   console.log(plus1);
+//   var minus1 = plus1.split('-');
+//   console.log(plus1);
+// }
