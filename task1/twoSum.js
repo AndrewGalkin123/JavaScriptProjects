5a35fc675a81f996b6dddf0865df45fed9ef1726
AndrewGalkin <127788127788a@gmail.com>
2023-01-09 22:52:16 +0200

    finished task#1

---
+++ task1/twoSum.js
@@ -0,0 +1,19 @@
+var twoSum = function(nums, target) {
+    for(var i = 0;i <= nums.length;i++){
+        var count = 1;
+        do{
+            if(target == nums[i] + nums[count] && count!==i){
+                var arr = [];
+                arr[i] = i;
+                arr[count] = count;
+                if(arr[count] == arr[i]){
+                    count++;
+                    arr[count] = count;
+                }
+                return  arr.filter(element => element !== null);
+            }
+            count++;
+        }while(count < nums.length)
+    }
+}
+console.log(twoSum([1,3,4,2],6));
