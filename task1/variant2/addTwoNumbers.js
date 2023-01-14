f49c69ba495a0995f54b2ec41665d6e846c6d11f
AndrewGalkin123 <127788127788a@gmail.com>
2023-01-14 11:59:27 +0200

    finished addTwoNumbers(second variant)

---
+++ task1/variant2/addTwoNumbers.js
@@ -0,0 +1,14 @@
+var addTwoNumbers = function(l1, l2) {
+    var first = [l1.val], second = [l2.val];
+    var node1 = l1.next, node2 = l2.next;
+    while(node1){
+        first.unshift(node1.val);
+        node1 = node1.next;
+    }
+    while(node2){
+        second.unshift(node2.val);
+        node2 = node2.next;
+    }
+    const res = (BigInt(first.join(``)) + BigInt(second.join(``))).toString();
+    return res.split(``).slice(1).reduce((head, val) => new ListNode(val, head), new ListNode(res[0]));
+};
