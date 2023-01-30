17af9812b7470aedb82fe49cb47f22cbf4330ac4
AndrewGalkin123 <127788127788a@gmail.com>
2023-01-30 18:23:17 +0200

    unfinished longestCommonPrefix

---
+++ longestCommonPrefixTask/longestCommonPrefix.js
@@ -0,0 +1,21 @@
+var longestCommonPrefix = function(strs) {
+    // var result = [];
+    // for (let i = 0; i < strs.length; i++) {
+    //     for (let j = 0; j < strs[i].length; j++) {
+    //         if(strs[i-1][j] === strs[i][j] && strs[i][j] === strs[i+1][j]){
+    //             console.log(strs[i-1][j]);
+    //             console.log(strs[i][j]);
+    //             console.log(strs[i+1][j]);
+    //             result += strs[i][j];
+    //         }
+    //     }
+    // }
+    // return result[0];
+    for (let i = 0; i < strs.length; i++) {
+        var count = 1;
+        do{
+
+        }while(count < strs.length)
+    }
+};
+console.log(longestCommonPrefix(["flower","flow","flight"]));
