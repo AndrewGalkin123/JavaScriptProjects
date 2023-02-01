22aa01f7d9eac8c5461a8886ece0cc8169e20c03
AndrewGalkin123 <127788127788a@gmail.com>
2023-02-01 12:34:27 +0200

    unfinished longestCommonPrefix

--- longestCommonPrefixTask/longestCommonPrefix.js
+++ longestCommonPrefixTask/longestCommonPrefix.js
@@ -11,19 +11,73 @@ var longestCommonPrefix = function (strs) {
     //     }
     // }
     // return result[0];
+    // var result = "";
+    // if(strs.length===1){
+    //     result += strs[0];
+    // }
+    // for (let i = 1; i < strs.length; i++) {
+    //     for (let j = 0; j < strs[i].length; j++) {
+    //         if (i !== strs.length - 1) {
+    //             if (strs[i][j] === strs[i + 1][j] && strs[i][j] === strs[i -1][j]) {
+    //                 result += strs[i][j];
+    //             }
+    //         }
+    //     }
+    // }
+    // return result;
+
+    // var result = "";
+    //
+    // function test(t) {
+    //     if (t === "undefined") {
+    //         return "";
+    //     }
+    //     return t;
+    // }
+    //
+    // if (strs.length === 1) {
+    //     result += strs[0];
+    // }
+    // for (let i = 1; i < strs.length; i++) {
+    //     for (let j = 0; j < strs[i].length; j++) {
+    //         if (i !== strs.length - 1) {
+    //             if (strs[i + 1][j] === strs[i][j] && strs[i - 1][j] === strs[i][j]) {
+    //                 result += strs[i][j];
+    //             }
+    //         }
+    //         if (strs.length === 2) {
+    //             if (strs[i - 1][j] === strs[i][j]) {
+    //                 result += strs[i][j];
+    //             }
+    //             if (strs[i] === strs[i - 1]) {
+    //                return strs[i];
+    //             }
+    //             if (strs[i - 1][j] !== strs[i][j]) {
+    //                 break;
+    //             }
+    //         }
+    //     }
+    // }
+    // return [...new Set(test(result))].join('');
     var result = "";
-    if(strs.length===1){
+
+    function test(t) {
+        if (t === "undefined") {
+            return "";
+        }
+        return t;
+    }
+    if (strs.length === 1) {
         result += strs[0];
     }
     for (let i = 1; i < strs.length; i++) {
-        for (let j = 0; j < strs[i].length; j++) {
-            if (i !== strs.length - 1) {
-                if (strs[i][j] === strs[i + 1][j] && strs[i][j] === strs[i -1][j]) {
-                    result += strs[i][j];
-                }
+        do {
+            if (strs[i + 1][i] === strs[i][i] && strs[i - 1][i] === strs[i][i]) {
+                result += strs[i][i];
             }
-        }
+
+        }while(i===strs.length)
     }
-    return result;
+    return test(result);
 };
-console.log(longestCommonPrefix(["ab", "a"]));
+console.log(longestCommonPrefix(["aaa", "aa", "aaa"]));
