83049c6ebca90e8c63ec21ea8e88730cea1b625c
AndrewGalkin123 <127788127788a@gmail.com>
2023-02-09 10:35:03 +0200

    finished removeElement

--- removeElement/removeElement.js
+++ removeElement/removeElement.js
@@ -1,5 +1,10 @@
-var count;
-var removeElement = function(nums, val) {
 
+var removeElement = function(nums, val) {
+    for (let i = 0; i < nums.length; i++) {
+        if(nums[i] === val){
+            nums = nums.filter((n) => {return n !== val});
+        }
+    }
+    console.log(nums);
 };
-console.log(removeElement())
+removeElement([3,2,2,3],3);
