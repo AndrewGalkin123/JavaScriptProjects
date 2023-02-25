6db921ff1be7894b4d0ffb1725e3fd8476ab60a8
AndrewGalkin123 <127788127788a@gmail.com>
2023-02-25 20:25:48 +0200

    selfdevelopment

--- nodeJS/main.js
+++ nodeJS/main.js
@@ -1,10 +1,10 @@
-const CC = require("currency-converter-lt");
-let currencyConverter = new CC({from:"USD", to:"EUR", amount:100})
+const os = require('os');
+let res = os.platform();
+console.log(res);
 
-
-currencyConverter.convert().then((response) => {
-    console.log("Result " + response);
-})
-
-
+const my_math = require('./my_math');
+let amount1 = my_math.add(4, 5);
+let amount2 = my_math.minus(10, 5);
+console.log(amount1);
+console.log(amount2);
 
---
+++ nodeJS/my_math.js
@@ -0,0 +1,11 @@
+const add = (a, b) => {
+    return a + b;
+}
+
+
+module.exports = {
+    add: add,
+    minus: (a, b) => {
+        return a - b;
+    }
+}
