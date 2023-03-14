79007de3f2ab599435b5f13c9d640c55ba91f07a
AndrewGalkin123 <127788127788a@gmail.com>
2023-03-14 21:37:32 +0200

    finished course "NodeJS"

--- nodeJS/expressJS/main.js
+++ nodeJS/expressJS/main.js
@@ -1,8 +1,11 @@
 const express = require('express')
 
+
 const app = express()
 
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.render('index')
@@ -11,6 +14,14 @@ app.get('/', (req, res) => {
 app.get('/user/:username', (req, res) => {
     let data = { username: req.params.username, hobbies: [ 'Football', 'Skate', 'Basketball' ] }
     res.render('user', data)
+})
+
+app.post('/check-user', (req, res) => {
+    let username = req.body.username;
+    if(username == "")
+        return res.redirect('/')
+    else    
+        return res.redirect('/user/' + username)
 })
 
 app.get('/about', (req, res) => {
