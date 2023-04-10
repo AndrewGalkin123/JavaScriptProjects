0513c4e69531316aaeac6b1e7dc1d366e208ff49
AndrewGalkin123 <127788127788a@gmail.com>
2023-04-10 21:06:35 +0300

    react-lesson5(selfdevelopment)

--- reactJS/my-app/src/App.js
+++ reactJS/my-app/src/App.js
@@ -4,18 +4,34 @@ import Image from './components/Image';
 import logo from "./img/logo-og.png"
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      helpText: "Help text",
+      userData : ""
+    }
+
+    this.inputClick = this.inputClick.bind(this)
+  }
+
+
   helpText = "Help Text"
   render() {
     return (<div className='name'>
       <Header title="Шапка сайта"/>
-      <h1>{this.helpText}</h1>
-      <input placeholder={this.helpText}
+      <h1>{this.state.helpText}</h1>
+      <h2>{this.state.userData}</h2>
+      <input placeholder={this.state.helpText}
+        onChange={event => this.setState({ userData : event.target.value})}
         onClick={this.inputClick} onMouseOver={this.mouseOver} />
-      <p>{this.helpText === "Help Text!" ? "Yes" : "No"}</p>
+      <p>{this.state.helpText === "Help Text" ? "Yes" : "No"}</p>
       <Image image={logo}/>
     </div>)
   }
-  inputClick() {console.log("Clicked")} 
+  inputClick() {
+    this.setState({ helpText : "Changed" })
+    console.log("Clicked")
+  } 
   mouseOver() {console.log("Mouse over")}
 }
 
