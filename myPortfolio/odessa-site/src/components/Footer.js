58fab6558d0a7decda482c55b422ffe87ad800ed
AndrewGalkin123 <127788127788a@gmail.com>
2023-05-18 23:17:29 +0300

    footer

--- myPortfolio/odessa-site/src/components/Footer.js
+++ myPortfolio/odessa-site/src/components/Footer.js
@@ -31,12 +31,7 @@ class Footer extends React.Component {
                                 <li>Телефон: (xxx)xxx-xx-xx</li>
                                 <li>Email: info@example.com</li>
                             </ul>
-                            <div class="SocialMedia">
-                                <a href="#" class="fa fa-instagram"></a>
-                                <a href="#" class="fa fa-facebook"></a>
-                                <a href="#" class="fa fa-twitter"></a>
-                                <a href="#" class="fa fa-google"></a>
-                            </div>
+                            
                         </div>
                     </div>
                 </Container>
