87ccc3ed987f6d7a4d55992c42b81e613524f5bd
AndrewGalkin123 <127788127788a@gmail.com>
2023-06-01 00:32:38 +0300

    filled block history

--- myPortfolio/odessa-site/src/components/History.js
+++ myPortfolio/odessa-site/src/components/History.js
@@ -1,6 +1,8 @@
 import React from "react";
 import pavelCathedral from 'D:/JavaScriptProjects/myPortfolio/odessa-site/src/files/img/pavelCathedral.jpg'
-import vorontsovLighthouse from 'D:/JavaScriptProjects/myPortfolio/odessa-site/src/files/img/vorontsov-lighthouse.jpg'
+import vorontsovLighthouse from 'D:/JavaScriptProjects/myPortfolio/odessa-site/src/files/img/lighthouse.jpg'
+import sailorWife from 'D:/JavaScriptProjects/myPortfolio/odessa-site/src/files/img/sailorWife.jpg'
+import opera from 'D:/JavaScriptProjects/myPortfolio/odessa-site/src/files/img/opera.jpg'
 
 class History extends React.Component {
     render() {
@@ -16,7 +18,7 @@ class History extends React.Component {
                 <br></br>
                 <div className="content" style={{ maxWidth: "1500px", display: "flex", flexWrap: "wrap", justifyContent: "space-around", padding: "0 15px" }}>
                     <a href="" className="box" style={{ margin: "15px", flex: "1 1 320px", minWidth: "300px", maxWidth: "100%", textDecoration: "none", color: "black" }}>
-                        <img src={pavelCathedral} style={{ width: "100%",  height: "470px", objectFit: "cover", objectPosition: "center"}} />
+                        <img src={pavelCathedral} style={{ width: "100%", height: "470px", objectFit: "cover", objectPosition: "center" }} />
                         <h2>Кафедральный Собор Св. Павла</h2>
                         <p>Величие веры и архитектурное великолепие.
                             Красивый фасад с колоннами и портиками, внутри – потрясающие фрески и орнаменты. Место поклонения и религиозных мероприятий, привлекающее туристов в Одессу.
@@ -24,27 +26,27 @@ class History extends React.Component {
                         </p>
                     </a>
                     <a href="" className="box" style={{ margin: "15px", flex: "1 1 320px", minWidth: "300px", maxWidth: "100%", textDecoration: "none", color: "black" }}>
-                        <img src={pavelCathedral} style={{ width: "100%", height: "470px", objectFit: "cover", objectPosition: "top" }} />
-                        <h2>Lorem ipsum dolor sit amet</h2>
-                        <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        <img src={sailorWife} style={{ width: "100%", height: "470px", objectFit: "cover", objectPosition: "top" }} />
+                        <h2>Памятник жене моряка</h2>
+                        <p>Трогательная история любви, запечатленная в бронзе. 
+                            Эта великолепная скульптура символизирует преданность и силу женщины, ожидающей возвращения моряка. 
+                            Загляните в глаза этой статуи и почувствуйте магию и эмоциональную привязанность, которые она передает.
 
                         </p>
                     </a>
                     <a href="" className="box" style={{ margin: "15px", flex: "1 1 320px", minWidth: "300px", maxWidth: "100%", textDecoration: "none", color: "black" }}>
-                        <img src={vorontsovLighthouse} style={{ width: "100%",  height: "470px", objectFit: "cover", objectPosition: "top"}} />
+                        <img src={vorontsovLighthouse} style={{ width: "100%", height: "470px", objectFit: "cover", objectPosition: "top" }} />
                         <h2>Воронцовский маяк</h2>
                         <p>Исторический символ и морская достопримечательность. Этот величественный маяк, построенный в 19 веке, предоставляет незабываемый вид на Чёрное море.
-                             Он служит ориентиром для мореплавателей и вдохновляет посетителей своей красотой и романтикой морской атмосферы.
+                            Он служит ориентиром для мореплавателей и вдохновляет посетителей своей красотой и романтикой морской атмосферы.
 
                         </p>
                     </a>
                     <a href="" className="box" style={{ margin: "15px", flex: "1 1 320px", minWidth: "300px", maxWidth: "100%", textDecoration: "none", color: "black" }}>
-                        <img src={pavelCathedral} style={{ width: "100%",  height: "470px", objectFit: "cover", objectPosition: "center"}} />
-                        <h2>Lorem ipsum dolor sit amet</h2>
-                        <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-
+                        <img src={opera} style={{ width: "100%", height: "470px", objectFit: "cover", objectPosition: "center" }} />
+                        <h2>Театральный сад</h2>
+                        <p>Уникальная оазисная зелень в центре города. Этот прекрасный парк является местом отдыха и культурных мероприятий.
+                            Здесь можно прогуляться среди красивых фонтанов, насладиться театральными представлениями и настроиться на волну искусства.
                         </p>
                     </a>
                 </div>
