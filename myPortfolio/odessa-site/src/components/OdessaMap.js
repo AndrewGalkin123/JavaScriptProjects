65411126979986231ad66522cecb242cd6b797ae
AndrewGalkin123 <127788127788a@gmail.com>
2023-06-08 23:47:19 +0300

    new variant realization of odessa map

--- myPortfolio/odessa-site/src/components/OdessaMap.js
+++ myPortfolio/odessa-site/src/components/OdessaMap.js
@@ -30,28 +30,14 @@ class OdessaMap extends React.Component {
                         className="content"
                         style={{ maxWidth: "1290px", textAlign: "center" }}
                     >
-                        <img
-                            id="odessaMap"
-                            src={odessaPhoto}
-                            alt="odessa-photo"
-                            style={{ maxWidth: "100%" ,width: "1290px"}}
-                        />
-                 
-                        <svg viewBox="0 0 1255 880" style={{ opacity: "0" }}>
-                            <path
-
-                                className="point"
-                                style={{ cursor: "pointer" }}
-                                d="m 532.86188,347.10796 c -11.28166,-18.89023 -11.54403,
-                            -17.31604 -11.28166,-18.89023 0.26236,-1.57418 -0.26237,-15.74186 12.33112,
-                            -14.43003 12.59349,1.31182 12.33112,5.50965 12.33112,5.50965 0,
-                            0 3.14837,4.46019 -5.50965,25.97406 -8.65802,21.51387 -18.89023,-15.74186 -18.89023,-15.74186 z"
-                                description-data="Одесса – город-порт на северо-западном побережье Черного моря. 
-                            Это третий по величине город Украины после Киева и Харькова, 
-                            главный промышленный, культурный, транспортный, научный и курортный центр Северного Причерноморья."
-                            ></path>
-                        </svg>
-                        <div className="description"></div>
+                        
+                        <iframe
+                            src="https://www.google.com/maps/d/u/0/embed?mid=14h5r74Wg4V75i8AC2_SOBeYQ_3mBcEE&ehbc=2E312F"
+                            width="100%"
+                            height="600"
+                            title="Google Map"
+                        ></iframe>
+                       
                     </div>
                 </div>
             </div>
