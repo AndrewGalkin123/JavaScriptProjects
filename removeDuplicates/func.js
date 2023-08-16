8c84e6bb73e5bab386e95dc52d2ca4263dbc09e9
AndrewGalkin123 <127788127788a@gmail.com>
2023-08-16 22:10:54 +0300

    finished leetcode task

--- removeDuplicates/func.js
+++ removeDuplicates/func.js
@@ -1,13 +1,29 @@
-var checkIfExist = function(arr) {
-    for(let i = 0; i < arr.length; i++){
-        for(let j = 0; j < arr.length; j++){
-           if(arr[i] === arr[j] * 2  && i !== j){
-               return true
-           }
+var validMountainArray = function(arr) {
+    if (arr.length < 3) {
+        return false;
+    }
+    
+    let increasing = 0;
+    let decreasing = 0;
+    
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > arr[i - 1]) {
+            if (decreasing === 0) {
+                increasing++;
+            } else {
+                return false;
+            }
+        } else if (arr[i] < arr[i - 1]) {
+            if (increasing > 0) {
+                decreasing++;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
         }
     }
     
-    return false
+    return increasing > 0 && decreasing > 0;
 };
-
-console.log(checkIfExist([0,0]))
+console.log(validMountainArray([3,5,5]))
