38541cc404e952459f6721bbffb27b265abde357
AndrewGalkin123 <127788127788a@gmail.com>
2023-08-17 20:57:12 +0300

    better solution to sort array

--- removeDuplicates/func.js
+++ removeDuplicates/func.js
@@ -1,14 +1,15 @@
 var moveZeroes = function(nums) {
-    let zeroes = []
-    let numbers = []
-    for (let i = 0; i < nums.length; i++) {
-        if(nums[i] == 0){
-            zeroes.push(nums[i])
-        }else{
-            numbers.push(nums[i])
+    
+    nums.sort(function(a, b) {
+        if (a === 0 && b !== 0) {
+          return 1; // Перемещаем нули в конец
+        } else if (a !== 0 && b === 0) {
+          return -1; // Остальные числа идут перед нулями
+        } else {
+          return 0; // Сохраняем порядок для равных чисел
         }
-    }
-   return  nums = numbers.concat(zeroes)
+      });
+      return nums;
 };
 
 console.log(moveZeroes([0,1,0,3,12]))
