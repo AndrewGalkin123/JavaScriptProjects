e2506a984a92740ad71ebbc2a0f1a7998b27ca0a
AndrewGalkin123 <127788127788a@gmail.com>
2023-09-13 20:50:40 +0300

    objects

--- experiments/main.js
+++ experiments/main.js
@@ -1,19 +1,19 @@
-function createIncrement() { 
-    let count = 0
-    function increment() {
-        count++
-    }
-    let message = 'Count is ' + count
-    function log() { 
-        console.log(message)
-    }
-    return [increment , log]
-}
-const [increment, log] = createIncrement()
-increment()
-increment()
-increment()
-log()
+// function createIncrement() { 
+//     let count = 0
+//     function increment() {
+//         count++
+//     }
+//     let message = 'Count is ' + count
+//     function log() { 
+//         console.log(message)
+//     }
+//     return [increment , log]
+// }
+// const [increment, log] = createIncrement()
+// increment()
+// increment()
+// increment()
+// log()
 
 // global  {
 //     link : null
@@ -42,3 +42,24 @@ log()
 // поєтому message count = 0 ; мы не переписываем
 // замыкание это поиск актуальных значений в коде
 // чтобы сделать все правильно и показывало актуальное значение нужно  let message = 'Count is ' + count переместить в log()
+
+const person = {
+    fullname:  function() {
+        return this.firstname + " " + this.lastname
+    }
+}
+
+const person1 = { 
+    firstname: "John",
+    lastname: "Doe"
+}
+
+const person2 = { 
+    firstname: "John2",
+    lastname: "Doe2"
+}
+
+console.log(person.fullname.call(person1))
+console.log(person.fullname.call(person2))
+
+
