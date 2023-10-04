505dddb1443ce88d10dad47f136348bcaa96fdfc
AndrewGalkin123 <127788127788a@gmail.com>
2023-10-04 20:18:44 +0300

    #

--- experiments/main.js
+++ experiments/main.js
@@ -1,20 +1,20 @@
 
-// function createIncrement() { 
-//     let count = 0
-//     function increment() {
-//         count++
-//     }
-//     let message = 'Count is ' + count
-//     function log() { 
-//         console.log(message)
-//     }
-//     return [increment , log]
-// }
-// const [increment, log] = createIncrement()
-// increment()
-// increment()
-// increment()
-// log()
+function createIncrement() { 
+    let count = 0
+    function increment() {
+        count++
+    }
+    let message = 'Count is ' + count
+    function log() { 
+        console.log(message)
+    }
+    return [increment , log]
+}
+const [increment, log] = createIncrement()
+increment()
+increment()
+increment()
+log()
 
 // global  {
 //     link : null
