8e595a7b26d4631546df9d9972bb28c2c1379bcd
AndrewGalkin123 <127788127788a@gmail.com>
2023-11-24 16:21:36 +0200

    ant design

--- antdesign/src/App.js
+++ antdesign/src/App.js
@@ -1,11 +1,24 @@
+import { useState } from "react";
+import { Row, Col, Slider, Typography } from "antd"
+
 import './App.css';
 import Header from './components/Header';
+import _Table from './components/Table';
 
 function App() {
+  const [rows, setRows] = useState(5)
+
   return (
-    <div>
+    <>
       <Header title="AntDesign Demo"></Header>
-    </div>
+      <Row>
+        <Col xs={24} md={{ span: 12, offset: 6 }}>
+          <Typography.Title level={4}>Количество покемонов на странице:</Typography.Title>
+          <Slider min={1} max={20} defaultValue={rows} onChange={setRows}></Slider>
+          <_Table rows={rows}></_Table>
+        </Col>
+      </Row>
+    </>
   );
 }
 
---
+++ antdesign/src/components/Table.jsx
@@ -0,0 +1,61 @@
+import { Table, Typography } from 'antd'
+import { pokemons } from "../data"
+
+const columns = [
+    {
+        title: "Name",
+        dataIndex: "name",
+        key: "name",
+        render: (text) => <Typography.Text copyable>{text}</Typography.Text>
+    },
+    {
+        title: "Number",
+        dataIndex: "number",
+        key: "number"
+    },
+    {
+        title: "Class",
+        dataIndex: "classification",
+        key: "classification",
+        filters: [
+            {
+                text: "Mouse Pokémon",
+                value: "Mouse Pokémon"
+            },
+            {
+                text: "Lizard Pokémon",
+                value: "Lizard Pokémon"
+            },
+            {
+                text: "Seed Pokémon",
+                value: "Seed Pokémon"
+            },
+        ],
+        onFilter: (value, item) => item.classification.includes(value)
+    },
+    {
+        title: "Rate",
+        dataIndex: "fleeRate",
+        key: "fleeRate",
+        sorter: (a, b) => a.fleeRate - b.fleeRate
+    }
+]
+
+const dataSource = pokemons.map(item => ({ ...item, key: item.id }))
+
+const _Table = ({rows = 5}) => {
+    return (
+        <Table
+            dataSource={dataSource}
+            columns={columns}
+            pagination={{
+                // defaultPageSize: 5,
+                // showSizeChanger: true,
+                // pageSizeOptions: [5, 10, 15, 20]
+                pageSize: rows
+            }}
+        />
+    )
+}
+
+export default _Table
