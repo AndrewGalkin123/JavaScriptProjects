7d71c515babde2a1ba0ce40f999359b0bffbd7d7
AndrewGalkin123 <127788127788a@gmail.com>
2024-01-12 12:25:43 +0200

    youtube video #1

---
+++ folder/index.js
@@ -0,0 +1,28 @@
+
+
+let name = "Andrew"
+
+let x = 5;
+let y = 10;
+
+let sum = x + y;
+console.log(sum)
+
+let age = 16
+
+if(age >= 18){
+    console.log('You are 18')
+}else {
+    console.log('You are not 18')
+}
+
+for(let i = 0; i <= 5; i++){
+    console.log(i)
+}
+
+function greetings(name){
+    console.log("Привет " + name)
+}
+
+greetings("John")
+
